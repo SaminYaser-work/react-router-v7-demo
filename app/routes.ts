8c130125593e032c4692fc5b995fb7bf1b63aaef
Samin Yaser <saminyaserwork@gmail.com>
2025-02-01 08:32:09 +0600

    first commit

---
+++ app/routes.ts
@@ -0,0 +1,7 @@
+/** @format */
+
+import { route, type RouteConfig } from "@react-router/dev/routes";
+
+export default [
+    route("contacts/:contactId", "routes/contact.tsx"),
+] satisfies RouteConfig;
